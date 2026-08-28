fd4b0d569825fa9326722b234011b0929f2ddbd1
Satvashil123 <gaonkarsatvashil@gmail.com>
2026-08-28 09:38:48 +0530

    Add Clerk Webhook handler to process user events and update database

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { application } from "express";
 import "dotenv/config";
 import cors from "cors";
 import fs from "fs";
@@ -8,11 +8,15 @@ import { clerkMiddleware } from '@clerk/express'
 import User from "./models/user.model.js";
 import connectDB from "./lib/db.js";
 import job from "./lib/cron.js";
+import { json } from "stream/consumers";
 
 const app = express();
 const PORT = process.env.PORT;
 const FRONTEND_URL = process.env.FRONTEND_URL;
 const publicDir = path.join(process.cwd(), "public");
+
+//it's important that you don't parse the webhook event data, it should be in the raw format
+app.use("/api/webhooks/clerk",express.raw({type:"application/json"}),clerkwebhook)
 app.use(express.json());
 app.use(cors({origin:FRONTEND_URL , credentials:true}));
 app.use(clerkMiddleware());
